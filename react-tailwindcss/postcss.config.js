7360c2b22a0ec942b15dc34d78d19aeddfb4068f
Roman Cherepanov <romach007@gmail.com>
2020-04-19 14:20:42 +0300

    Integrate Tailwind.css to React App

---
+++ react-tailwindcss/postcss.config.js
@@ -0,0 +1,16 @@
+console.log(process.env.NODE_ENV)
+
+module.exports = {
+    plugins: [
+      require('tailwindcss'),
+      require('autoprefixer'),
+      process.env.NODE_ENV === 'production' && require('@fullhuman/postcss-purgecss')({
+        content: [
+          './src/**/*.jsx',
+          './src/**/*.js',
+          './public/index.html',
+        ],
+        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+      })
+    ]
+  }
---
+++ react-tailwindcss/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className=" text-blue-500">
+      Hello!!!
+    </div>
+  );
+}
+
+export default App;
